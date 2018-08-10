35de91d58c504b20b3f32f0ca3cfae1657ff65da
heliohachimine <helio_hachimine_jr@hotmail.com>
2018-08-10 10:56:40 -0300

    injetando um serviço no outro serviço

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CursosModule } from './cursos/cursos.module';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
-
+import { LogService} from './shared/log.service'; 
+ 
 @NgModule({
   declarations: [
     AppComponent
@@ -16,9 +17,8 @@ import { CriarCursoModule } from './criar-curso/criar-curso.module';
     CriarCursoModule,
     CursosModule
   ],
-  providers: [
-    CursosService
-  ],
+  //providers: [CursosService],
+  providers: [LogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
